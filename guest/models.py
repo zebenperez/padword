e664f4993863c2048caf3546c11aa26fd71ba8cb
Dani Diaz <danidiaz@gmail.com>
2021-07-07 22:43:49 +0000

    Dani

--- guest/models.py
+++ guest/models.py
@@ -10,6 +10,74 @@ class Guest(models.Model):
 #     channel = models.ForeignKey('web.Channel', to_field='uuid', on_delete=models.SET_NULL, null=True)
 #     project = models.ForeignKey('web.Project', to_field='uuid', on_delete=models.SET_NULL, null=True)
     PID = models.IntegerField(verbose_name='PID')
+    UUID = models.CharField(max_length=255, verbose_name='UUID')
+    room = models.CharField(max_length=255, verbose_name='Room')
+    name = models.CharField(max_length=255, verbose_name='Name')
+    surname = models.CharField(max_length=255, verbose_name='Surname')
+    project_id = models.CharField(max_length=255, verbose_name='Project')
+    language = models.CharField(max_length=255, verbose_name='Language')
+    check_in = models.DateTimeField(verbose_name='Check-In')
+    check_out = models.DateTimeField(verbose_name='Check-Out')
+
+    @property
+    def project(self):
+        try:
+            return Project.objects.get(uuid = self.project_id)
+        except Exception as e:
+            return Project(name='UNKNOWN')
+
+    @property
+    def channels(self):
+        try:
+            channels_uuid = GuestByChannel.objects.filter(UUID = self.UUID).values_list('channel_id', flat=True)
+            items = Channel.objects.none()
+            for uuid in channels_uuid:
+                items = items.union(Channel.objects.filter(uuid=uuid))
+            return (items)
+        except Exception as e:
+            print (show_exc(e))
+            return Channel.objects.none()
+
+    @classmethod
+    def by_project(cls, projects):
+        try:
+            items = Guest.objects.none()
+            if isinstance(projects, models.query.QuerySet):
+                for project_uuid in projects.all().values_list('uuid', flat=True):
+                    items = items.union(Guest.objects.filter(project_id = project_uuid))
+            else:
+                items = Guest.objects.filter(project_id = projects.uuid)
+            return items
+        except Exception as e:
+            print (show_exc(e))
+            return Guest.objects.none()
+
+    @classmethod
+    def by_channel(cls, channels):
+        try:
+            items = GuestByChannel.objects.none()
+            if isinstance(channels, models.query.QuerySet):
+                for channel_uuid in channels.all().values_list('uuid', flat=True):
+                    items = items.union(GuestByChannel.objects.filter(channel_id = channel_uuid))
+            else:
+                items = GuestByChannel.objects.filter(channel_id = channels.uuid)
+            items = Guest.objects.filter(UUID__in = items.all().values_list('UUID', flat=True))
+            return items
+        except Exception as e:
+            print (show_exc(e))
+            return Guest.objects.none()
+        
+
+    class Meta:
+        managed = False
+        db_table = 'guests'
+        verbose_name = _('Guest')
+
+class GuestByChannel(models.Model):
+#     channel = models.ForeignKey('web.Channel', to_field='uuid', on_delete=models.SET_NULL, null=True)
+#     project = models.ForeignKey('web.Project', to_field='uuid', on_delete=models.SET_NULL, null=True)
+    PID = models.IntegerField(verbose_name='PID')
+    UUID = models.CharField(max_length=255, verbose_name='UUID')
     room = models.CharField(max_length=255, verbose_name='Room')
     name = models.CharField(max_length=255, verbose_name='Name')
     surname = models.CharField(max_length=255, verbose_name='Surname')
@@ -66,4 +134,3 @@ class Guest(models.Model):
         managed = False
         db_table = 'guests-by-channel'
         verbose_name = _('Guest')
-
