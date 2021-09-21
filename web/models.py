843d3d3eddced50b79d54a9c7f1c37869f4ffe5a
Dani Diaz <danidiaz@gmail.com>
2021-09-21 14:58:43 +0000

    Zeben. Remove languages from debug

--- web/models.py
+++ web/models.py
@@ -47,7 +47,7 @@ class Project(models.Model):
     @property
     def get_languages(self):
         try:
-            print (self.used_languages)
+            #print (self.used_languages)
             return (self.used_languages.upper().replace(' ','').split(','))
         except Exception as e:
             print (show_exc(e))
