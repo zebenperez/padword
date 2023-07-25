2a65bae556f3750daa16657ad9bb11736b4a485c
Zeben Perez <zeben@vps-d65b23f4.vps.ovh.net>
2023-07-25 13:09:50 +0000

    ajuste conector avantio

--- connector/avantio_lib.py
+++ connector/avantio_lib.py
@@ -214,7 +214,7 @@ def room_exist(project_uuid, room):
     count = Room.objects.filter(project_uuid=project_uuid, number=room).count()
     return (count > 0)
 
-def create_booking(pau, booking, start_date, s_date, e_date):
+def create_booking(pau, av, booking, start_date, s_date, e_date):
     checkin = get_date(booking.start_date, booking.start_time, start_date)
     room = room_exist(pau.project_uuid, booking.accommodation_code)
     if booking.client.name != "" and booking.client.surname != "" and checkin >= s_date and checkin <= e_date and room:
@@ -249,7 +249,7 @@ def get_booking_list(project_uuid):
         av = ShAvantio(pau.username, pau.password)
         booking_list = av.get_booking_list(start_date, end_date)
         for booking in booking_list:
-            create_booking(pau, booking, start_date, start_date_new, end_date_new)
+            create_booking(pau, av, booking, start_date, start_date_new, end_date_new)
 
             #if booking.client.name != "" and booking.client.surname != "":
             #    checkin = get_date(booking.start_date, booking.start_time, start_date)
@@ -285,7 +285,7 @@ def get_booking_notif(project_uuid):
         booking_list = av.get_booking_notifications()
         for booking in booking_list:
             b = av.get_booking(booking.booking_code, booking.localizator)
-            create_booking(pau, b, start_date, start_date_new, end_date_new)
+            create_booking(pau, av, b, start_date, start_date_new, end_date_new)
 
             #if b != None and b.client.name != "" and b.client.surname != "":
             #    checkin = get_date(b.start_date, b.start_time, start_date)
