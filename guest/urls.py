8ee70d20894276235e863270de1fbc65c131c577
Zeben Perez <zeben@vps-842e4710.vps.ovh.net>
2023-09-06 12:31:30 +0000

    ajuste nuevo huesped desde admin local

--- guest/urls.py
+++ guest/urls.py
@@ -25,8 +25,8 @@ urlpatterns = [
     path('guests/project/remove/', views.guest_remove_by_project, name='guest-remove-by-project'),
     path('guests/project/soft-remove/<int:obj_id>/', views.guest_soft_remove_by_project, name='guest-soft-remove-by-project'),
     path('guests/project/soft-remove-all/', views.guest_soft_remove_all_by_project, name='guest-soft-remove-all-by-project'),
-    path('guests/project/details/<int:obj_id>/', views.guest_details_by_project, name='guests-details-by-project'),
-    path('guests/project/details/', views.guest_details_by_project, name='guests-details-by-project'),
+    path('guests/project/details/<int:obj_id>/', views.guest_details_by_project, name='guest-details-by-project'),
+    path('guests/project/details/', views.guest_details_by_project, name='guest-details-by-project'),
 
     path('guests/update-code/', views.guest_update_code, name='guest-update-code'),
     path('guests/save-date/', views.guest_save_date, name='guest-save-date'),
