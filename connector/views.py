a85ae666a0cc9a6a75aa497f713a60e5988f39e7
Zeben Perez <zeben@vps-d65b23f4.vps.ovh.net>
2023-11-07 13:20:44 +0000

    ajuste logs

--- connector/views.py
+++ connector/views.py
@@ -82,7 +82,11 @@ def avantio_send_link(request, project_uuid, guest_uuid):
 def avantio_log(request):
     f = open(os.path.join(settings.BASE_DIR, "avantio.log"), "r", encoding='utf-8')
     text = f.read()
-    return render(request, 'cron-log.html', {'text': text.replace("\n", "<br/>"),})
+    try:
+        log_list = os.listdir(settings.LOGPATH)
+    except:
+        log_list = []
+    return render(request, 'cron-log.html', {'text': text.replace("\n", "<br/>"), 'log_list': log_list})
 
 '''
     Avaibook
@@ -141,7 +145,11 @@ def avaibook_get_booking(request):
 def avaibook_log(request):
     f = open(os.path.join(settings.BASE_DIR, "avaibook.log"), "r", encoding='utf-8')
     text = f.read()
-    return render(request, 'cron-log.html', {'text': text.replace("\n", "<br/>"),})
+    try:
+        log_list = os.listdir(settings.LOGPATH)
+    except:
+        log_list = []
+    return render(request, 'cron-log.html', {'text': text.replace("\n", "<br/>"), 'log_list': log_list})
 
 '''
     Winhotel
@@ -196,7 +204,11 @@ def winhotel_import_items(request, project_uuid):
 def cron_log(request):
     f = open(os.path.join(settings.BASE_DIR, "cron.log"), "r", encoding='utf-8')
     text = f.read()
-    return render(request, 'cron-log.html', {'text': text.replace("\n", "<br/>"),})
+    try:
+        log_list = os.listdir(settings.LOGPATH)
+    except:
+        log_list = []
+    return render(request, 'cron-log.html', {'text': text.replace("\n", "<br/>"), 'log_list'})
 
 @group_required("admins")
 def test_email(request):
