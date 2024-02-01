93268c4959a534d72b8d80f18cdf432227fd20e8
Zeben Perez <zeben@vps-d65b23f4.vps.ovh.net>
2024-02-01 09:37:02 +0000

    busqueda por id en pedidos y diseño

--- bookings/tpv_orders_views.py
+++ bookings/tpv_orders_views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.utils.translation import ugettext_lazy as _ 
 
@@ -5,6 +6,7 @@ from padword.decorators import group_required
 from padword.commons import show_exc, get_or_none, get_param, get_float, get_bool, new_ui_slug, get_items_per_page
 from web.models import Project
 from contents.models import Category
+from guest.models import Guest
 
 from .models import Form, FormInstance, FormInstanceInfo, Status
 
@@ -18,7 +20,18 @@ ITEMS_PER_PAGE=get_items_per_page()
 '''
     Project users
 '''
-def search(project_uuid, ini_date, end_date, name, status):
+def filter_search_guest(name, uuid_project_list):
+    uuid_guests = list(Guest.objects.filter(Q(name__icontains=name) | Q(surname__icontains=name) | Q(email__icontains=name) | Q(mobile__icontains=name) | Q(room=name)).filter(project_id__in = uuid_project_list).values_list('UUID', flat=True))
+    return uuid_guests
+
+
+def search(project_uuid, ini_date, end_date, name, status, s_id=""):
+    if s_id != "":
+        try:
+            return FormInstance.objects.filter(id=s_id)
+        except Exception as e:
+            print(e)
+
     form = Form.objects.filter(form_type__code="tpv", form_type__project_uuid=project_uuid).first()
 
     kwargs = {'form_uuid': form.uuid}
@@ -27,8 +40,8 @@ def search(project_uuid, ini_date, end_date, name, status):
     if end_date != "":
         ed = end_date.split("-")
         kwargs["date__lte"] = datetime.datetime(int(ed[0]), int(ed[1]), int(ed[2]), 23, 59, 59)
-    #if name != "":
-    #    kwargs["guest_uuid__in"] = filter_search_guest(name, [project_uuid])
+    if name != "":
+        kwargs["guest_uuid__in"] = filter_search_guest(name, [project_uuid])
 
     return FormInstance.objects.filter(**kwargs)
     #items = FormInstance.objects.filter(**kwargs)
@@ -55,13 +68,13 @@ def get_orders_project_context(project):
 def orders_search(request):
     try:
         project = get_or_none(Project, request.project_id)
-        #form = get_param(request.GET, "s-form")
+        s_id = get_param(request.GET, "s-id")
         ini_date = get_param(request.GET, "s-ini_date")
         end_date = get_param(request.GET, "s-end_date")
         name = get_param(request.GET, "s-name")
         status = get_param(request.GET, "s-status")
 
-        items = search(project.uuid, ini_date, end_date, name, status)
+        items = search(project.uuid, ini_date, end_date, name, status, s_id)
 
         context={'total_items': len(items), 'items': items[0:ITEMS_PER_PAGE], 'status': status, 'index': ITEMS_PER_PAGE}
         return render(request, "bookings/tpv-orders/order-list.html", context)
