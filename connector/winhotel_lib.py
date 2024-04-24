f414966392bd92f0ee0b4cf0cfde478a01d469eb
Zeben Perez <zeben@vps-842e4710.vps.ovh.net>
2024-04-24 12:26:52 +0000

    conector de winhotel

--- connector/winhotel_lib.py
+++ connector/winhotel_lib.py
@@ -233,10 +233,10 @@ class Winhotel:
     def _request_bookings(self, state):
         json = {"QueryCredentials": self._credentials(), "UserID": self.user_id}
         _bookings_params = {
-            "BookingStateQueryParameters": {
+            "BookingStateQueryParameters": [{
                 "QueryOperator": 0,
                 "Value": state
-            }
+            }]
         }
         json["QueryRequest"] = {"QueryHeader": self._request_header(), "BookingListQueryParameters": _bookings_params}
         return json
@@ -248,10 +248,10 @@ class Winhotel:
                 "QueryOperator": date_operator,
                 "Value": start_date
             },
-            "BookingStateQueryParameters": {
+            "BookingStateQueryParameters": [{
                 "QueryOperator": 0,
                 "Value": state
-            },
+            }],
         }
         json["QueryRequest"] = {"QueryHeader": self._request_header(), "BookingListQueryParameters": _bookings_params}
         return json
@@ -263,10 +263,10 @@ class Winhotel:
                 "QueryOperator": 0,
                 "Value": date
             },
-            "BookingStateQueryParameters": {
+            "BookingStateQueryParameters": [{
                 "QueryOperator": 0,
                 "Value": state
-            },
+            }],
         }
         json["QueryRequest"] = {"QueryHeader": self._request_header(), "BookingListQueryParameters": _bookings_params}
         return json
