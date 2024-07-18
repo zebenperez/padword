83d4fb33a958467a548c3f308f48eaea43d85781
Zeben Perez <zeben@vps-d65b23f4.vps.ovh.net>
2024-07-18 10:51:54 +0000

    ajuste mensajes al añadir tarjeta desde listado de huéspedes

--- guest/views.py
+++ guest/views.py
@@ -905,7 +905,7 @@ def key_add_card_all(request):
         name = "{} {}".format(guest.name, guest.surname)
 
         err = guest.add_all_key_card(code, name)
-        msg =_("Card added successfully") if err == "" else "Card not added! {}".format(err)
+        msg =_("Card added successfully") if err == "" or "Error" not in err else "Card not added! {}".format(err)
         return render(request, "guest/keys/guest-keys-all-msg.html", {"msg": msg})
         #return HttpResponse(msg);
         #return render(request, "guest/keys/guest-keys-all.html", {"obj": guest, 'msg': True})
