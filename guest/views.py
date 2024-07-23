22be56ed30d61ded98db45924d36f56fe11ab654
zeben <zebenperez@shidix.com>
2024-07-23 07:29:40 +0000

    solucionado problema al añadir pulsera desde el listado de huespedes

--- guest/views.py
+++ guest/views.py
@@ -905,11 +905,12 @@ def key_add_card_all(request):
         name = "{} {}".format(guest.name, guest.surname)
 
         err = guest.add_all_key_card(code, name)
-        msg =_("Card added successfully") if err == "" or "Error" not in err else "Card not added! {}".format(err)
+        msg =_("Card added successfully") if str(err) == "" or "Error" not in str(err) else "Card not added! {}".format(err)
         return render(request, "guest/keys/guest-keys-all-msg.html", {"msg": msg})
         #return HttpResponse(msg);
         #return render(request, "guest/keys/guest-keys-all.html", {"obj": guest, 'msg': True})
     except Exception as e:
+        print(e)
         return render(request, "error_exception.html", {'exc':show_exc(e)})
 
 
