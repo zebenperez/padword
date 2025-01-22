669f3228f810a8dcac84fbec67b9986ef0507b7d
Zeben Perez <zeben@vps-d65b23f4.vps.ovh.net>
2025-01-22 09:36:14 +0000

    acceso a pwa de huésped por id externo

--- bookings/urls.py
+++ bookings/urls.py
@@ -89,6 +89,7 @@ urlpatterns = [
     #------------- Bookings Guests --------------#
     path('guest-access/<slug:category_uuid>/', gv.guest_access, name='guest-access'),
     path('guest-access-auto/<slug:guest_uuid>/', gv.guest_access_auto, name='guest-access-auto'),
+    path('guest-access-ext/<slug:ext_id>/', gv.guest_access_ext, name='guest-access-ext'),
     #path('guest-access-anonymous/<slug:category_uuid>/', gv.guest_access_anonymous, name='guest-access-anonymous'),
     path('guest-set-lang/', gv.set_guest_language, name='guest-set-lang'),
     #path('guest-set-lang/<slug:category_uuid>/<str:lang>/', gv.set_guest_language, name='guest-set-lang'),
