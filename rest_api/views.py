c7287204722ae88f395ec702451b2721b1f8638a
Zeben Perez <zeben@vps-d65b23f4.vps.ovh.net>
2026-03-27 19:26:11 +0000

    ajuste fechas get_tickets2

--- rest_api/views.py
+++ rest_api/views.py
@@ -749,8 +749,7 @@ class TicketViewSet(viewsets.ViewSet):
             if form != None:
                 logger.info("[DEBUG]: PROCESANDO")
                 res = form.to_tickets2(start_date, end_date)
-                logger.info(f'[DEBUG]: PROCESADO')
-                logger.info(f'[DEBUG]: PROCESADO {len(res)}')
+                logger.info(f'[DEBUG]: PROCESADO [{res}]')
                 return Response(res)
             return Response({"error": True, 'msg': 'This project do not have TPV configured!'})
         except Exception as e:
