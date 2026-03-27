fae32cacb16c2f76e1d76d4e1f48eebc87a9c29d
Zeben Perez <zeben@vps-d65b23f4.vps.ovh.net>
2026-03-27 14:10:26 +0000

    log api

--- rest_api/views.py
+++ rest_api/views.py
@@ -740,6 +740,8 @@ class TicketViewSet(viewsets.ViewSet):
     @action(detail=False, methods=['POST'])
     def get_tickets2(self, request):
         try:
+            import time
+            t0 = time.time()
             logger.info("[DEBUG]: ({}) ENTRANDO EN GET TICKETS2".format(request.META.get('REMOTE_ADDR')))
             pu = ProjectUser.objects.get(username=self.request.user.username)
             start_date = request.POST["start_date"] if "start_date" in request.POST else ""
@@ -749,7 +751,7 @@ class TicketViewSet(viewsets.ViewSet):
             if form != None:
                 logger.info("[DEBUG]: PROCESANDO")
                 res = form.to_tickets2(start_date, end_date)
-                logger.info("[DEBUG]: {}".format(res))
+                logger.info("[DEBUG]: {} ({})".format(res, (time.time()-t0)))
                 return Response(res)
             return Response({"error": True, 'msg': 'This project do not have TPV configured!'})
         except Exception as e:
