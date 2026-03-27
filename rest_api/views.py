edc7179bb8322292c79aa974f93f9b2642f78864
Zeben Perez <zeben@vps-d65b23f4.vps.ovh.net>
2026-03-27 14:07:17 +0000

    log api

--- rest_api/views.py
+++ rest_api/views.py
@@ -746,9 +746,11 @@ class TicketViewSet(viewsets.ViewSet):
             end_date = request.POST["end_date"] if "end_date" in request.POST else ""
 
             form = Form.objects.filter(form_type__code="tpv", form_type__project_uuid=pu.project.uuid).first()
-            logger.info("[DEBUG]: PROCESADO")
             if form != None:
-                return Response(form.to_tickets2(start_date, end_date))
+                logger.info("[DEBUG]: PROCESANDO")
+                res = form.to_tickets2(start_date, end_date)
+                logger.info("[DEBUG]: {}".format(res))
+                return Response(res)
             return Response({"error": True, 'msg': 'This project do not have TPV configured!'})
         except Exception as e:
             logger.error("[{}]: \"{}\"".format(self.request.user, str(e)))
