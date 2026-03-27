0cf7a2130e0b4ec96460b19ccd62bc6f2ddadfc7
Zeben Perez <zeben@vps-d65b23f4.vps.ovh.net>
2026-03-27 19:05:19 +0000

    ajuste fechas get_tickets2

--- rest_api/views.py
+++ rest_api/views.py
@@ -740,7 +740,7 @@ class TicketViewSet(viewsets.ViewSet):
     @action(detail=False, methods=['POST'])
     def get_tickets2(self, request):
         try:
-            logger.info("[DEBUG]: ({}) ENTRANDO EN GET TICKETS2: {}".format(request.META.get('REMOTE_ADDR'), request.POST))
+            logger.info("[DEBUG]: ({}) ENTRANDO EN GET TICKETS2: {} {}".format(request.META.get('REMOTE_ADDR'), request.META.get('HTTP_AUTHORIZATION'), request.POST))
             pu = ProjectUser.objects.get(username=self.request.user.username)
             start_date = request.POST["start_date"] if "start_date" in request.POST else ""
             end_date = request.POST["end_date"] if "end_date" in request.POST else ""
