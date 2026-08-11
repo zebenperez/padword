39123ecd25f9aa746027322ca4e31a1112ac976d
Zeben Perez <zeben@vps-d65b23f4.vps.ovh.net>
2026-08-11 23:36:37 +0000

    ajuste api close bands

--- rest_api/views.py
+++ rest_api/views.py
@@ -386,7 +386,7 @@ class GuestViewSet(viewsets.ModelViewSet):
                 c_out = datetime.strptime(check_out, "%Y-%m-%d %H:%M:%S")
             else:
                 c_out = get_today_end() 
-                c_out = (c_out + timedelta(days=1)).replace(hour=1, minute=0, second=0, microsecond=0)
+                c_out = pu.project.local_date(c_out + timedelta(days=1)).replace(hour=1, minute=0, second=0, microsecond=0)
             c_in = pu.project.local_date(datetime.now()).strftime("%Y-%m-%d %H:%M:%S")
             data = { 
                 "UUID": new_ui_slug(Guest, "UUID"), 
